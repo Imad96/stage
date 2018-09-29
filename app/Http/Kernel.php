1e7dd7e97f49e152ae32a11b6e66077b0015bdf5
Azidane Messaoud <mesutzidan@gmail.com>
2018-09-29 13:33:12 +0100

    admin auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,5 +53,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'ajax' => \App\Http\Middleware\Ajax::class,
+        'admin' => \App\Http\Middleware\Admin::class,
+
     ];
 }
